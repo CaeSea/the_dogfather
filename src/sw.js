d926fb87356e329568b790fd92cd818c3f72593f
Caeron <caeking93@gmail.com>
2019-07-30 19:21:08 +0100

    Made service worker fully operational

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,8 @@
+const staticCacheName = "dogfather-cache-v1";
+
 self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open("theDogfather").then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         "./",
         "./index.html",
@@ -40,10 +42,34 @@ self.addEventListener("install", function(e) {
   );
 });
 
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function(cacheName) {
+            return (
+              cacheName.startsWith("dogfather-") && cacheName != staticCacheName
+            );
+          })
+          .map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+      );
+    })
+  );
+});
+
 self.addEventListener("fetch", function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
   );
+});
+
+self.addEventListener("message", function(event) {
+  if (event.data.action == "skipWaiting") {
+    self.skipWaiting();
+  }
 });
